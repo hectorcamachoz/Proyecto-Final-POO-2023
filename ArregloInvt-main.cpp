23d81903a16d964f42e3cece1173a17dae67392e
isapelle <114602640+isapelle@users.noreply.github.com>
2023-05-19 17:18:53 -0600

    Update ArregloInvt-main.cpp

--- ArregloInvt-main.cpp
+++ ArregloInvt-main.cpp
@@ -6,7 +6,10 @@ using namespace std;
 #include "ArregloInvt.h"
 
 int main(){
+    cout << "FUnciona" << endl;
     ArregloInvt objeto;
+    cout << "Funciona por favor" << endl;
+    system("pause");
     objeto.menuInvt();
     return 0;
 }
